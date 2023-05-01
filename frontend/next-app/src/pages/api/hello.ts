d8d48497dfdd173f6a040cda0afbe980f125295a
Alexandre Pernin <alexandrep@theodo.fr>
2023-05-02 00:19:21 +0200

    chore: test env variables on vercel

--- frontend/next-app/src/pages/api/hello.ts
+++ frontend/next-app/src/pages/api/hello.ts
@@ -2,10 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
+  url: string;
 };
 
 const hello = (_req: NextApiRequest, res: NextApiResponse<Data>): void => {
-  res.status(200).json({ name: 'John Doe' });
+  res
+    .status(200)
+    .json({ name: 'John Doe', url: process.env.NEXT_PUBLIC_URL ?? '' });
 };
 
 export default hello;
