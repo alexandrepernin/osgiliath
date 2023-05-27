c255e691402556dc3d47934654ca20b7a69a203c
Alexandre Pernin <alexandrep@theodo.fr>
2023-05-27 14:56:41 +0200

    feat: wip test next auth

--- frontend/next-app/src/pages/_app.tsx
+++ frontend/next-app/src/pages/_app.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ChakraProvider } from '@chakra-ui/react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { Session, SessionContextProvider } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { useState } from 'react';
 
 const App = ({
   Component,
-  pageProps,
+  // @ts-ignore test next js auth
+  pageProps: { session: nextAuthSession, ...pageProps },
 }: AppProps<{
   initialSession: Session;
 }>): JSX.Element => {
@@ -18,9 +21,11 @@ const App = ({
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <SessionProvider session={nextAuthSession}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
     </SessionContextProvider>
   );
 };
