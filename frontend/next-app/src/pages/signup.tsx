e86f4d5297f883e37dec0de39c0cd1af6340060e
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-05-31 08:49:30 +0200

    feat: add google signin on signup page

--- frontend/next-app/src/pages/signup.tsx
+++ frontend/next-app/src/pages/signup.tsx
@@ -2,11 +2,12 @@ import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
+  Center,
+  Divider,
   Flex,
   FormControl,
   FormLabel,
   Heading,
-  HStack,
   Input,
   InputGroup,
   InputRightElement,
@@ -20,9 +21,12 @@ import NextLink from 'next/link';
 import { Pages } from 'constants/pages';
 import { useSignup } from 'hooks/useSignup';
 import { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { useGoogleSignin } from 'hooks/useGoogleSignin';
 
 const Page = (): JSX.Element => {
   const [showPassword, setShowPassword] = useState(false);
+  const { signinWithGoogle } = useGoogleSignin();
   const { handleInputChange, onSubmit, formValues } = useSignup();
 
   return (
@@ -35,7 +39,7 @@ const Page = (): JSX.Element => {
       <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6}>
         <Stack align="center">
           <Heading fontSize="4xl" textAlign="center">
-            Sign up
+            Sign up to your account
           </Heading>
           <Text fontSize="lg" color="gray.600">
             to enjoy all of our cool features ✌️
@@ -47,77 +51,77 @@ const Page = (): JSX.Element => {
           boxShadow="lg"
           p={8}
         >
-          <form onSubmit={event => void onSubmit(event)}>
-            <Stack spacing={4}>
-              <HStack>
-                <Box>
-                  <FormControl id="firstName">
-                    <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
-                </Box>
-                <Box>
-                  <FormControl id="lastName">
-                    <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
-                </Box>
-              </HStack>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input
-                  required
-                  type="email"
-                  name="email"
-                  value={formValues.email}
-                  onChange={handleInputChange}
-                  placeholder="Email address"
-                />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
+          <Stack spacing={4}>
+            <Button
+              w="full"
+              maxW="md"
+              variant="outline"
+              leftIcon={<FcGoogle />}
+              onClick={() => void signinWithGoogle()}
+            >
+              <Center>
+                <Text>Sign up with Google</Text>
+              </Center>
+            </Button>
+            <Divider />
+            <form onSubmit={event => void onSubmit(event)}>
+              <Stack spacing={4}>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
                   <Input
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    value={formValues.password}
+                    required
+                    type="email"
+                    name="email"
+                    value={formValues.email}
                     onChange={handleInputChange}
-                    placeholder="Password"
+                    placeholder="Email address"
                   />
-                  <InputRightElement h="full">
-                    <Button
-                      variant="ghost"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                <Button
-                  loadingText="Submitting"
-                  size="lg"
-                  bg="gray.700"
-                  color="white"
-                  _hover={{
-                    bg: 'gray.600',
-                  }}
-                  type="submit"
-                >
-                  Sign up
-                </Button>
+                </FormControl>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={showPassword ? 'text' : 'password'}
+                      name="password"
+                      value={formValues.password}
+                      onChange={handleInputChange}
+                      placeholder="Password"
+                    />
+                    <InputRightElement h="full">
+                      <Button
+                        variant="ghost"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText="Submitting"
+                    size="lg"
+                    bg="gray.700"
+                    color="white"
+                    _hover={{
+                      bg: 'gray.600',
+                    }}
+                    type="submit"
+                  >
+                    Sign up
+                  </Button>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align="center">
+                    Already a user?{' '}
+                    <Link as={NextLink} color="gray.600" href={Pages.SIGNIN}>
+                      Login
+                    </Link>
+                  </Text>
+                </Stack>
               </Stack>
-              <Stack pt={6}>
-                <Text align="center">
-                  Already a user?{' '}
-                  <Link as={NextLink} color="gray.600" href={Pages.SIGNIN}>
-                    Login
-                  </Link>
-                </Text>
-              </Stack>
-            </Stack>
-          </form>
+            </form>
+          </Stack>
         </Box>
       </Stack>
     </Flex>
