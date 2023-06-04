6a80cce5235e5925be5a9fd3c11c2d7ff88976f8
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-06-04 18:52:26 +0200

    feat: use a html template for the forgot password email

--- frontend/next-app/src/services/emails/sendForgotPassword.ts
+++ frontend/next-app/src/services/emails/sendForgotPassword.ts
@@ -1,19 +1,34 @@
+import { readFileSync } from 'fs';
+import Handlebars from 'handlebars';
+import path from 'path';
 import { transporter } from './constants';
+
+const emailsDir = path.resolve(process.cwd(), 'src/emails');
 
 export const sendForgotPassword = async (
   email: string,
   token: string,
 ): Promise<void> => {
   try {
-    await Promise.resolve();
-    const url = `${
+    const emailFile = readFileSync(
+      path.join(emailsDir, 'forgot-password-email.html'),
+      {
+        encoding: 'utf8',
+      },
+    );
+    const emailTemplate = Handlebars.compile(emailFile);
+    const resetPasswordUrl = `${
       process.env.NEXT_PUBLIC_URL ?? ''
     }reset-password?token=${token}&email=${encodeURI(email)}`;
     await transporter.sendMail({
-      from: process.env.SMTP_AUTH_USER,
-      to: [email],
-      subject: 'Reset your password',
-      text: `Click on this link to reset your password: ${url}`,
+      from: `"🏝️ Malibou" ${process.env.SMTP_AUTH_USER ?? ''}`,
+      to: email,
+      subject: 'Your reset password link for Malibou',
+      html: emailTemplate({
+        base_url: process.env.NEXTAUTH_URL,
+        reset_url: resetPasswordUrl,
+        email,
+      }),
     });
   } catch (error) {
     console.error(error);
