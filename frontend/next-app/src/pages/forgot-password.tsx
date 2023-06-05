169ca1dcc96aa59248c6d03fecdf74adb490820b
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-06-05 09:58:25 +0200

    feat: display confirmation message once forgot password email is sent

--- frontend/next-app/src/pages/forgot-password.tsx
+++ frontend/next-app/src/pages/forgot-password.tsx
@@ -1,4 +1,6 @@
+import { CheckCircleIcon } from '@chakra-ui/icons';
 import {
+  Box,
   Button,
   Flex,
   FormControl,
@@ -7,7 +9,6 @@ import {
   Input,
   Stack,
   Text,
-  useColorModeValue,
 } from '@chakra-ui/react';
 import { useForgotPassword } from 'hooks/useForgotPassword';
 import { useForm } from 'react-hook-form';
@@ -21,25 +22,28 @@ const Page = (): JSX.Element => {
   const {
     handleSubmit,
     register,
-    formState: { errors, isSubmitting },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm<ForgotPasswordFormData>();
 
   const { onSubmit } = useForgotPassword();
 
+  if (isSubmitSuccessful) {
+    return (
+      <Flex minH="100vh" align="center" justify="center">
+        <Success />
+      </Flex>
+    );
+  }
+
   return (
     /* eslint-disable-next-line @typescript-eslint/no-misused-promises */
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Flex
-        minH="100vh"
-        align="center"
-        justify="center"
-        bg={useColorModeValue('gray.50', 'gray.800')}
-      >
+      <Flex minH="100vh" align="center" justify="center" bg="gray.50">
         <Stack
           spacing={4}
           w="full"
           maxW="md"
-          bg={useColorModeValue('white', 'gray.700')}
+          bg="white"
           rounded="xl"
           boxShadow="lg"
           p={6}
@@ -48,10 +52,7 @@ const Page = (): JSX.Element => {
           <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
             Forgot your password?
           </Heading>
-          <Text
-            fontSize={{ base: 'sm', sm: 'md' }}
-            color={useColorModeValue('gray.800', 'gray.400')}
-          >
+          <Text fontSize={{ base: 'sm', sm: 'md' }} color="gray.800">
             You&apos;ll get an email with a reset link
           </Text>
           <FormControl
@@ -82,6 +83,20 @@ const Page = (): JSX.Element => {
         </Stack>
       </Flex>
     </form>
+  );
+};
+
+const Success = () => {
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <CheckCircleIcon boxSize="50px" color="green.500" />
+      <Heading as="h2" size="xl" mt={6} mb={2}>
+        Please check your mailbox
+      </Heading>
+      <Text color="gray.500">
+        We have sent you an email with a link to reset your password.
+      </Text>
+    </Box>
   );
 };
 
