ce07e4b71cf9857f8e8670aefb2d1f9342b8f717
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-06-16 16:36:08 +0200

    feat: use clerk components and remove unused code

--- frontend/next-app/src/pages/index.tsx
+++ frontend/next-app/src/pages/index.tsx
@@ -1,29 +1,12 @@
 import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
 
 import { SidebarWithHeader } from 'components/Sidebar';
-import { Pages } from 'constants/pages';
 
 const Home = (): JSX.Element => {
-  const router = useRouter();
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated: () => {
-      void router.push(Pages.SIGNIN);
-    },
-  });
-
   return (
-    <>
-      {status === 'loading' ? (
-        <></>
-      ) : (
-        <SidebarWithHeader>
-          <>{JSON.stringify(session.user ?? '')}</>
-        </SidebarWithHeader>
-      )}
-    </>
+    <SidebarWithHeader>
+      <></>
+    </SidebarWithHeader>
   );
 };
 
