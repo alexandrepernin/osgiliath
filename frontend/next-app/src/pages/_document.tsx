0f1d8776f97d04590e0f9d74b176be434d106666
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-07-01 11:51:34 +0200

    feat: add favicon

--- frontend/next-app/src/pages/_document.tsx
+++ frontend/next-app/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = (): JSX.Element => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏄</text></svg>"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
