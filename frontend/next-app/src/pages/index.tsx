4e1c545814e479b0b4802630835f6b94279b49d3
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-07-01 16:27:44 +0200

    feat: display users from current organization on homepage

--- frontend/next-app/src/pages/index.tsx
+++ frontend/next-app/src/pages/index.tsx
@@ -1,11 +1,30 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 import { SidebarWithHeader } from 'components/Sidebar';
 import { EmployeeListing } from 'components/EmployeeListing';
 import { Button, Stack } from '@chakra-ui/react';
 import { FiPlus } from 'react-icons/fi';
+import { getAuth } from '@clerk/nextjs/server';
+import { User } from '@prisma/client';
+import { getOrganizationMembers } from 'services/database/organization';
 
-const Home = (): JSX.Element => {
+export const getServerSideProps: GetServerSideProps<{
+  users: User[];
+}> = async ctx => {
+  const { orgId } = getAuth(ctx.req);
+  if (orgId === null || orgId === undefined) {
+    return { props: { users: [] } };
+  }
+  const users = await getOrganizationMembers(orgId);
+
+  return {
+    props: { users },
+  };
+};
+
+const Page = ({
+  users,
+}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
   return (
     <SidebarWithHeader menuIndex={0}>
       <Stack direction="row" spacing={4} justify="flex-end" marginBottom="3">
@@ -13,17 +32,9 @@ const Home = (): JSX.Element => {
           Employee
         </Button>
       </Stack>
-      <EmployeeListing />
+      <EmployeeListing users={users} />
     </SidebarWithHeader>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  await Promise.resolve();
-
-  return {
-    props: {},
-  };
-};
-
-export default Home;
+export default Page;
