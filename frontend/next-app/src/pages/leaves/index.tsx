af46ed572d83f8dd3c655c5b116ffb4d9e126678
Alexandre Pernin <alex.e.pernin@gmail.com>
2023-07-09 00:04:41 +0200

    fix: use client directive for chakra provider

--- frontend/next-app/src/pages/leaves/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SidebarWithHeader } from 'components/Sidebar';
-
-const Page = (): JSX.Element => {
-  return <SidebarWithHeader menuIndex={2}>Leaves</SidebarWithHeader>;
-};
-
-export default Page;
